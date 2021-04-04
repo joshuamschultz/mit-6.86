af096426cdac6387add2df3cd4cd8bcaaf278a72
Joshua Schultz <joshuamschultz@gmail.com>
2021-04-04 11:20:24 -0500

    cluster algo

--- mnist/part2-twodigit/conv.py
+++ mnist/part2-twodigit/conv.py
@@ -19,11 +19,24 @@ class CNN(nn.Module):
 
     def __init__(self, input_dimension):
         super(CNN, self).__init__()
-        # TODO initialize model layers here
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.dropout = nn.Dropout(0.5)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
 
     def forward(self, x):
 
-        # TODO use model layers to predict the two digits
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 5 * 5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        out_first_digit = x[0]
+        out_second_digit = x[1]
 
         return out_first_digit, out_second_digit
 
